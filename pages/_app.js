40189d0132981172688dac0a71a29c09095602cc
Ruben Thys <rubenthys22@gmail.com>
2022-05-02 01:18:02 +0200

    Initialise project

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '@fontsource/poppins/700.css';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
+
+export default MyApp;
